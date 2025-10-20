fffe8cf35eea3f9d10ab158f495d9766840bc990
VaazzDan <danilovazcam@gmail.com>
2025-10-20 14:09:53 -0300

    ajuste identação

--- src/Exercicio3.java
+++ src/Exercicio3.java
@@ -5,13 +5,13 @@ public class Exercicio3 {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         System.out.println("Informe seu nome:");
-        var nomePrimeiraPessoa = scanner.next();
+            var nomePrimeiraPessoa = scanner.next();
         System.out.println("Informe sua idade");
-        var idadePrimeiraPessoa = scanner.nextInt();
+            var idadePrimeiraPessoa = scanner.nextInt();
         System.out.println("Informe seu nome:");
-        var nomeSegundaPessoa = scanner.next();
+            var nomeSegundaPessoa = scanner.next();
         System.out.println("Informe sua idade");
-        var idadeSegundaPessoa = scanner.nextInt();
+            var idadeSegundaPessoa = scanner.nextInt();
         var diferencaIdade = idadePrimeiraPessoa - idadeSegundaPessoa;
         System.out.printf("A diferença de idade entre %s e %s é de %s anos\n", nomePrimeiraPessoa, nomeSegundaPessoa, diferencaIdade);
         
